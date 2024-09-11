e0577d529664ba54f8d96640698d6291364846e0
Rens de Wit <r.dewit1@students.uu.nl>
2024-09-11 18:51:31 +0200

    Add comments to Attempt.cs

--- Predatorprey/Attempt.cs
+++ Predatorprey/Attempt.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Project1
 {
+    /// <summary>
+    /// Class made for the Success function
+    /// </summary>
     public class Attempt
     {
+        /// <summary>
+        /// Returns whether a random draw failed or succeeded
+        /// </summary>
+        /// <param name="rnd">A random generator</param>
+        /// <param name="chance">The chance that the random draw succeeds</param>
+        /// <returns>Whether the draw succeeded or not</returns>
         public static bool Success(Random rnd, double chance) => rnd.NextDouble() < chance;
     }
 }
